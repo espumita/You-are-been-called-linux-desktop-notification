4d506ab907c685efa0327a0c98bf20e5774780e1
espumitadk <davidfalconnaranjo@gmail.com>
2017-05-26 18:31:30 +0100

    clean and tips

--- app/src/main/java/com/example/david/callwatcher/AnswerCallBroadcastReceiver.java
+++ app/src/main/java/com/example/david/callwatcher/AnswerCallBroadcastReceiver.java
@@ -5,7 +5,6 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.telephony.TelephonyManager;
-import android.widget.EditText;
 
 import org.fusesource.mqtt.client.BlockingConnection;
 import org.fusesource.mqtt.client.MQTT;
@@ -18,40 +17,29 @@ import static org.fusesource.hawtbuf.UTF8Buffer.utf8;
 
 public class AnswerCallBroadcastReceiver extends BroadcastReceiver{
 
-    private static String HOST ="";
+    private static String HOST = "tcp://192.168.1.37:1883";
     private static final String TOPIC = "call";
-    private MQTT mqttClient;
 
     @Override
     public void onReceive(Context context, Intent intent) {
         if(intent.getAction().equals("android.intent.action.PHONE_STATE")){
             String state = intent.getStringExtra(TelephonyManager.EXTRA_STATE);
-            if(state.equals(TelephonyManager.EXTRA_STATE_OFFHOOK)){
-            }
-            else if (state.equals(TelephonyManager.EXTRA_STATE_RINGING)){
+            if (state.equals(TelephonyManager.EXTRA_STATE_RINGING)){
                 String number = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
                 try {
-                    loadHost();
                     BlockingConnection connection = setUpConnection();
                     connection.connect();
                     connection.subscribe(getTopics());
-                    publish(TelephonyManager.EXTRA_INCOMING_NUMBER, connection);
+                    publish(number, connection);
                     connection.disconnect();
-                } catch (Exception e) {
+                } catch (Exception e) { }
+            }
 
-                }
-            }
-            else if(state.equals(TelephonyManager.EXTRA_STATE_IDLE)){
-            }
         }
-    }
-    private void loadHost() {
-        HOST ="tcp://192.168.1.37:1883";
     }
 
     private BlockingConnection setUpConnection() throws URISyntaxException {
-        mqttClient = new MQTT();
-        mqttClient.setClientId("1234");
+        MQTT mqttClient = new MQTT();
         mqttClient.setHost(HOST);
         return mqttClient.blockingConnection();
     }
